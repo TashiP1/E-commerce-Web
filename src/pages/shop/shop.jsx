dad7ecc54c1a2c349d72365c2fce2af8cdc6348e
Tashi Phuntsho <tashitakuley@gmail.com>
2023-10-31 17:49:58 +0600

    Search feature

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,20 +1,47 @@
 import "./shop.css";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 
 export const Shop = () => {
+    const [search, setSearch] = useState("");
+    const [filterItem, setFilterItem] = useState(PRODUCTS);
+  
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        const filterdata = PRODUCTS.filter((item) =>
+          item.productName.toLowerCase().includes(e.target.value.toLowerCase())
+        );
+        setFilterItem(filterdata);
+    };
+  
     return (
-        <div className="shop">
-            <div className="shopTitle">
-                <h1>
-                    BLACK AND <span>WHITE</span>
-                </h1>
-            </div>
-            <div className="products">
-                {PRODUCTS.map((product) => (
-                    <Product key={product.id} data={product} />
-                ))}
-            </div>
+      <div className="shop">
+        <div className="shopTitle">
+          <h1>
+            BLACK AND <span>WHITE</span>
+          </h1>
         </div>
+        <div className="search-container">
+          <form>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-input"
+              value={search}
+              onChange={handleSearch}
+            />
+            <button type="submit" className="search-button">
+              <FaSearch className="search-icon" />
+            </button>
+          </form>
+        </div>
+        <div className="products">
+          {filterItem.map((product) => (
+            <Product key={product.id} data={product} />
+          ))}
+        </div>
+      </div>
     );
 };
